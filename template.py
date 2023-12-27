5c0118d258ee13094a173135d33935c84afbc9ff
brian leung <brian.leung@idnerd.com>
2023-12-27 17:21:22 +0800

    add template

---
+++ template.py
@@ -0,0 +1,14 @@
+import sys
+sys.setrecursionlimit(10**5+5)
+input = sys.stdin.readline
+
+def solve():
+    n = int(input())
+    a = [int(input()) for _ in range(n)]
+
+if __name__ == "__main__":
+    test = 1
+    # test = int(input())
+    while(test):
+        solve()
+        test-=1
