69f7a8b02506b6206578e66134cd5afd1681f6e4
Brian Leung <knleungaa@connect.ust.hk>
2024-02-19 22:23:52 +0800

    udpate

--- question/leetcode/contest/wc384/q4.py
+++ question/leetcode/contest/wc384/q4.py
@@ -12,68 +12,30 @@ class Solution:
             if a[i] < a[i-1]:
                 pre.append(-1)
 
-        def KMPSearch(pat, txt):
-            M = len(pat)
-            N = len(txt)
-            ans = 0
-        
-            # create lps[] that will hold the longest prefix suffix
-            # values for pattern
-            lps = [0]*M
-            j = 0  # index for pat[]
-        
-            # Preprocess the pattern (calculate lps[] array)
-            computeLPSArray(pat, M, lps)
-        
-            i = 0  # index for txt[]
-            while (N - i) >= (M - j):
-                if pat[j] == txt[i]:
-                    i += 1
-                    j += 1
-        
-                if j == M:
-                    ans += 1
-                    j = lps[j-1]
-        
-                # mismatch after j matches
-                elif i < N and pat[j] != txt[i]:
-                    # Do not match lps[0..lps[j-1]] characters,
-                    # they will match anyway
-                    if j != 0:
-                        j = lps[j-1]
-                    else:
-                        i += 1
+        p.append(2)
 
-            return ans
+        dp = [0]
+        for i in range(1,len(p)):
+            j = dp[i-1]
+            while j and p[j] != p[i]:
+                j = dp[j-1]
+            if p[j] == p[i]:
+                j+=1
+            dp.append(j)
         
-        
-        # Function to compute LPS array
-        def computeLPSArray(pat, M, lps):
-            len = 0  # length of the previous longest prefix suffix
-        
-            lps[0] = 0  # lps[0] is always 0
-            i = 1
-        
-            # the loop calculates lps[i] for i = 1 to M-1
-            while i < M:
-                if pat[i] == pat[len]:
-                    len += 1
-                    lps[i] = len
-                    i += 1
-                else:
-                    # This is tricky. Consider the example.
-                    # AAACAAAA and i = 7. The idea is similar
-                    # to search step.
-                    if len != 0:
-                        len = lps[len-1]
-        
-                        # Also, note that we do not increment i here
-                    else:
-                        lps[i] = 0
-                        i += 1
+        # print(p)
+        # print(dp)
 
-        return KMPSearch(p, pre)
+        ans = 0
+        j = 0
+        for i in range(len(pre)):
+            while j and p[j] != pre[i]:
+                j = dp[j-1]
+            if p[j] == pre[i]:
+                j+=1
+            # print(j, end=" ")
+            if j == len(p)-1:
+                ans+=1
+        # print(ans)
 
-
-    
-
+        return ans
