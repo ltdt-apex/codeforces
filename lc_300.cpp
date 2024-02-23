e5e447c8767bad0a7d9bc7c4eef99c1a1303947f
brian <knleungaa@connect.ust.hk>
2024-02-23 18:32:58 +0800

    udpate

---
+++ lc_300.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<int> vi;
+ 
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<pair<ll,ll>> vll;
+typedef vector<vector<pair<ll,ll>>> vvll;
+typedef vector<vector<vector<ll>>> vvvl;
+typedef vector<tuple<ll, ll, ll>> vlll;
+ 
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define range(i,b) for (ll i = 0; i < b; i++)
+#define rep(i,a,b) for (ll i = a; i < b; i++)
+#define repr(i,a,b) for (ll i = a; i > b; i--)
+#define sq(a) (a)*(a)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define ceild(a,b) (a)%(b) ? ((a)/(b) + 1) : (a)/(b)
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout)
+ 
+const ll mod = 1e9 + 7;
+ 
+const int au = 65;
+const int al = 97;
+const int zu = 90;
+const int zl = 122;
+ 
+const int lt = 32;
+ 
+// dsu O(log n) find_set and union_sets
+pair<vl,vl> init_set(ll n){
+    vl parent(n);
+    rep(i,0,n){
+        parent[i] = i;
+    }
+    vl size(n, 1);
+ 
+    return {parent, size};
+}
+ 
+void make_set(ll v, vl& parent, vl& size) {
+    parent[v] = v;
+    size[v] = 1;
+}
+ 
+ll find_set(ll v, vl& parent) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v], parent);
+}
+ 
+ll same_set(ll u, ll v, vl& parent){
+    return find_set(u, parent) == find_set(v, parent);
+}
+ 
+void union_sets(ll a, ll b, vl& parent, vl& size) {
+    a = find_set(a, parent);
+    b = find_set(b, parent);
+    if (a != b) {
+        if (size[a] < size[b])
+            swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+    }
+}
+ 
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& a) {
+        int n = a.size();
+
+        vl dp;
+
+        for (auto x: a){
+            ll i = lower_bound(dp.begin(), dp.end(), x) - dp.begin();
+
+            if (i==dp.size()){
+                dp.pb(x);
+            }
+            else{
+                dp[i] = x;
+            }
+        }
+
+        return dp.size();
+    }
+};
