ccf38cc760f33576cb21d6d32ee09907a957a0b0
Brian Leung <knleungaa@connect.ust.hk>
2024-07-02 16:37:44 -0400

    update leetcode script

--- setup_lc_contest.sh
+++ setup_lc_contest.sh
@@ -2,7 +2,7 @@
 
 template_file="template/template.cpp"
 leetcode_contest_folder="question/leetcode/contest"
-contest="weekly-contest-401"
+contest=$1
 
 leetcode_contest_dir=$leetcode_contest_folder/$contest
 
@@ -11,10 +11,10 @@ sed -i "/^void solve/,/^}/d" temp.cpp
 sed -i "/^int main/,/^}/d" temp.cpp
 
 mkdir -p $leetcode_contest_dir
-cp temp.cpp $leetcode_contest_dir/q1.cpp
-cp temp.cpp $leetcode_contest_dir/q2.cpp
-cp temp.cpp $leetcode_contest_dir/q3.cpp
-cp temp.cpp $leetcode_contest_dir/q4.cpp
+cp -n temp.cpp $leetcode_contest_dir/q1.cpp
+cp -n temp.cpp $leetcode_contest_dir/q2.cpp
+cp -n temp.cpp $leetcode_contest_dir/q3.cpp
+cp -n temp.cpp $leetcode_contest_dir/q4.cpp
 
 rm temp.cpp
 
