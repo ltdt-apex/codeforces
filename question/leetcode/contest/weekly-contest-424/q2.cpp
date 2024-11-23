0485b220c3c8ba2b0b0c49a2cff281cea88a32df
brian.leung <knleungaa@connect.ust.hk>
2024-11-22 22:55:57 -0500

    wc424 partial

---
+++ question/leetcode/contest/weekly-contest-424/q2.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+
+typedef pair<int, int> pii;
+typedef vector<pair<int,int>> vii;
+typedef vector<vector<pair<int,int>>> vvii;
+typedef vector<tuple<int, int, int>> viii;
+
+typedef tuple<int, int, int> tiii;
+
+typedef long long ll;
+
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+
+typedef pair<ll, ll> pll;
+typedef vector<pair<ll,ll>> vll;
+typedef vector<vector<pair<ll,ll>>> vvll;
+typedef vector<tuple<ll, ll, ll>> vlll;
+
+typedef tuple<ll, ll, ll> tlll;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define range(i,b) for (ll i = 0; i < b; i++)
+#define rep(i,a,b) for (ll i = a; i < b; i++)
+#define repr(i,a,b) for (ll i = a; i > b; i--)
+#define sq(a) (a)*(a)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define ceild(a,b) ((a)%(b) ? ((a)/(b) + 1) : (a)/(b))
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout)
+
+const ll mod = 1e9 + 7;
+
+class Solution {
+public:
+    bool isZeroArray(vector<int>& v, vector<vector<int>>& qs) {
+        int n = v.size();
+        vi dp(n+1);
+
+        rep(i,0,qs.size()){
+            int l = qs[i][0];
+            int r = qs[i][1];
+
+            dp[l]++;
+            dp[r+1]--;
+        }
+
+        int t = 0;
+
+        rep(i,0,n){
+            t+=dp[i];
+            if(v[i]>t) return false;
+        }
+
+        return true;
+    }
+};
+
