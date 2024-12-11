4089fe16eafccf1642f2715030b271b3c62e0c67
brian.leung <knleungaa@connect.ust.hk>
2024-12-11 15:16:46 -0500

    i am a mess

---
+++ question/leetcode/daily/2024/dec/find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+
+typedef pair<int, int> pii;
+typedef vector<pair<int,int>> vii;
+typedef vector<vector<pair<int,int>>> vvii;
+typedef vector<tuple<int, int, int>> viii;
+
+typedef tuple<int, int, int> tiii;
+
+typedef long long ll;
+
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+
+typedef pair<ll, ll> pll;
+typedef vector<pair<ll,ll>> vll;
+typedef vector<vector<pair<ll,ll>>> vvll;
+typedef vector<tuple<ll, ll, ll>> vlll;
+
+typedef tuple<ll, ll, ll> tlll;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define range(i,b) for (ll i = 0; i < b; i++)
+#define rep(i,a,b) for (ll i = a; i < b; i++)
+#define repr(i,a,b) for (ll i = a; i > b; i--)
+#define sq(a) (a)*(a)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define ceild(a,b) ((a)%(b) ? ((a)/(b) + 1) : (a)/(b))
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout)
+
+const ll mod = 1e9 + 7;
+
+vii dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+vii dirs8 = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+
+
+class Solution {
+public:
+    int maximumLength(string s) {
+        int n = s.size();
+
+        int a = -1;
+
+        rep(i,0,n){
+            rep(j,i,n){
+                char c = s[i];
+                int sp = 1;
+                rep(k,i,j+1){
+                    if(s[k]!=c) sp = 0;
+                }
+
+                if(not sp) continue;
+
+                int count = 0;
+                for(int k=i;k+j-i<n;k++){
+
+                    int valid = 1;
+
+                    for(int l=0;l<j-i+1;l++){
+                        if(s[i+l] != s[k+l]) valid = 0;
+                    }
+
+                    if(valid) count++;
+                }
+
+                if(count==3) a = max(a,int(j-i+1));
+            }
+        }
+
+        return a;
+    }
+};
