bc2828481d821c7c486c2d93b2af217ac2602059
brian.leung <knleungaa@connect.ust.hk>
2025-01-09 15:17:10 -0500

    i heard that full commit tree can earn a free job is that true?

---
+++ question/leetcode/daily/2025/jan/count-prefix-and-suffix-pairs-i.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+
+typedef pair<int, int> pii;
+typedef vector<pair<int,int>> vii;
+typedef vector<vector<pair<int,int>>> vvii;
+typedef vector<tuple<int, int, int>> viii;
+
+typedef tuple<int, int, int> tiii;
+
+typedef long long ll;
+
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+
+typedef pair<ll, ll> pll;
+typedef vector<pair<ll,ll>> vll;
+typedef vector<vector<pair<ll,ll>>> vvll;
+typedef vector<tuple<ll, ll, ll>> vlll;
+
+typedef tuple<ll, ll, ll> tlll;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define range(i,b) for (ll i = 0; i < b; i++)
+#define rep(i,a,b) for (ll i = a; i < b; i++)
+#define repr(i,a,b) for (ll i = a; i > b; i--)
+#define sq(a) (a)*(a)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define ceild(a,b) ((a)%(b) ? ((a)/(b) + 1) : (a)/(b))
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout)
+
+const ll mod = 1e9 + 7;
+
+vii dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+vii dirs8 = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+
+
+class Solution {
+public:
+    int countPrefixSuffixPairs(vector<string>& v) {
+        int n = v.size();
+
+        ll a = 0;
+        rep(i,0,n){
+            string s1 = v[i];
+            int m1 = s1.size();
+            rep(j,i+1,n){
+                string s2 = v[j];
+                int m2 = s2.size();
+                
+                if(m2<m1) continue;
+
+                int ok = 1;
+                rep(k,0,m1){
+                    if(s1[k] != s2[k]) ok = 0;
+                }
+                rep(k,0,m1){
+                    if(s1[m1-1-k] != s2[m2-1-k]) ok = 0;
+                }
+
+                if(ok)a++;
+            }
+        }
+
+        return a;
+    }
+};
---
+++ question/leetcode/daily/2025/jan/shifting-letters-ii.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+
+typedef pair<int, int> pii;
+typedef vector<pair<int,int>> vii;
+typedef vector<vector<pair<int,int>>> vvii;
+typedef vector<tuple<int, int, int>> viii;
+
+typedef tuple<int, int, int> tiii;
+
+typedef long long ll;
+
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+
+typedef pair<ll, ll> pll;
+typedef vector<pair<ll,ll>> vll;
+typedef vector<vector<pair<ll,ll>>> vvll;
+typedef vector<tuple<ll, ll, ll>> vlll;
+
+typedef tuple<ll, ll, ll> tlll;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define range(i,b) for (ll i = 0; i < b; i++)
+#define rep(i,a,b) for (ll i = a; i < b; i++)
+#define repr(i,a,b) for (ll i = a; i > b; i--)
+#define sq(a) (a)*(a)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define ceild(a,b) ((a)%(b) ? ((a)/(b) + 1) : (a)/(b))
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout)
+
+const ll mod = 1e9 + 7;
+
+vii dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+vii dirs8 = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+
+
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& v) {
+        int n = s.size();
+
+        vi p(n+1);
+
+        int m = v.size();
+
+        rep(i,0,m){
+            int l = v[i][0];
+            int r = v[i][1];
+            int inc = v[i][2];
+
+            if(inc == 0) inc = -1;
+
+            p[l]+=inc;
+            p[r+1]-=inc;
+        }
+
+        rep(i,0,n){
+            s[i] = (s[i]-'a' + 26*n + p[i])%26 + 'a';
+            p[i+1] += p[i];
+        }
+
+        return s;
+
+    }
+};
