50b3cea4be53d61e04137ce754fa053f0cd79e53
brian.leung <knleungaa@connect.ust.hk>
2025-02-06 10:48:35 -0500

    daily

---
+++ question/leetcode/daily/2025/feb/tuple-with-same-product.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+
+typedef pair<int, int> pii;
+typedef vector<pair<int,int>> vii;
+typedef vector<vector<pair<int,int>>> vvii;
+typedef vector<tuple<int, int, int>> viii;
+
+typedef tuple<int, int, int> tiii;
+
+typedef long long ll;
+
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+
+typedef pair<ll, ll> pll;
+typedef vector<pair<ll,ll>> vll;
+typedef vector<vector<pair<ll,ll>>> vvll;
+typedef vector<tuple<ll, ll, ll>> vlll;
+
+typedef tuple<ll, ll, ll> tlll;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define range(i,b) for (ll i = 0; i < b; i++)
+#define rep(i,a,b) for (ll i = a; i < b; i++)
+#define repr(i,a,b) for (ll i = a; i > b; i--)
+#define sq(a) (a)*(a)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define ceild(a,b) ((a)%(b) ? ((a)/(b) + 1) : (a)/(b))
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout)
+
+const ll mod = 1e9 + 7;
+
+vii dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+vii dirs8 = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+
+// int C(int n, int k) {
+//     double res = 1;
+//     for (int i = 1; i <= k; ++i)
+//         res = res * (n - k + i) / i;
+//     return (int)(res + 0.01);
+// }
+
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& v) {
+        int n = v.size();
+
+        unordered_map<int, int> m;
+
+        rep(i,0,n){
+            rep(j,i+1,n){
+                m[v[i]*v[j]]++;
+            }
+        }
+
+        int a = 0;
+
+        for(auto [k,v]: m){
+            // cout << v << endl;
+            if(v>1) a+=v*(v-1)*4;
+        }
+
+        return a;
+    }
+};
