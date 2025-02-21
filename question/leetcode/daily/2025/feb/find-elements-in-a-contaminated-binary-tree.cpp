1182fb0c23143f975e283b29bc129ae2ad5ac5d8
brian.leung <knleungaa@connect.ust.hk>
2025-02-21 07:25:26 -0500

    daily

---
+++ question/leetcode/daily/2025/feb/find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+
+typedef pair<int, int> pii;
+typedef vector<pair<int,int>> vii;
+typedef vector<vector<pair<int,int>>> vvii;
+typedef vector<tuple<int, int, int>> viii;
+
+typedef tuple<int, int, int> tiii;
+
+typedef long long ll;
+
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+
+typedef pair<ll, ll> pll;
+typedef vector<pair<ll,ll>> vll;
+typedef vector<vector<pair<ll,ll>>> vvll;
+typedef vector<tuple<ll, ll, ll>> vlll;
+
+typedef tuple<ll, ll, ll> tlll;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define range(i,b) for (ll i = 0; i < b; i++)
+#define rep(i,a,b) for (ll i = a; i < b; i++)
+#define repr(i,a,b) for (ll i = a; i > b; i--)
+#define sq(a) (a)*(a)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define ceild(a,b) ((a)%(b) ? ((a)/(b) + 1) : (a)/(b))
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout)
+
+const ll mod = 1e9 + 7;
+
+vii dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+vii dirs8 = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+    private:
+        unordered_set<int> s;
+        void dfs(TreeNode* node, int x){
+            if(not node) return;
+
+            s.insert(x);
+            dfs(node->left, x*2+1);
+            dfs(node->right, x*2+2);
+        }
+    public:
+        FindElements(TreeNode* root) {
+            dfs(root, 0);
+            
+        }
+        
+        bool find(int target) {
+            return s.count(target);
+        }
+    };
+    
+    /**
+     * Your FindElements object will be instantiated and called as such:
+     * FindElements* obj = new FindElements(root);
+     * bool param_1 = obj->find(target);
+     */
