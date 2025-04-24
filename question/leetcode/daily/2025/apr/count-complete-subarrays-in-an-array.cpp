864d7616359265f01229d5da1563f4f0a030a7b1
brian.leung <knleungaa@connect.ust.hk>
2025-04-24 12:56:29 -0400

    daily

---
+++ question/leetcode/daily/2025/apr/count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+
+typedef pair<int, int> pii;
+typedef vector<pair<int,int>> vii;
+typedef vector<vector<pair<int,int>>> vvii;
+typedef vector<tuple<int, int, int>> viii;
+
+typedef tuple<int, int, int> tiii;
+
+typedef long long ll;
+
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+
+typedef pair<ll, ll> pll;
+typedef vector<pair<ll,ll>> vll;
+typedef vector<vector<pair<ll,ll>>> vvll;
+typedef vector<tuple<ll, ll, ll>> vlll;
+
+typedef tuple<ll, ll, ll> tlll;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define range(i,b) for (ll i = 0; i < b; i++)
+#define rep(i,a,b) for (ll i = a; i < b; i++)
+#define repr(i,a,b) for (ll i = a; i > b; i--)
+#define sq(a) (a)*(a)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define ceild(a,b) ((a)%(b) ? ((a)/(b) + 1) : (a)/(b))
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout)
+
+const ll mod = 1e9 + 7;
+
+vii dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+vii dirs8 = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+
+
+class Solution {
+    public:
+        int countCompleteSubarrays(vector<int>& v) {
+            int n = v.size();
+            unordered_map<int,int> m;
+
+            rep(i,0,n){
+                m[v[i]]++;
+            }
+
+            int c = m.size();
+            unordered_map<int,int> m2;
+
+            int r = 0;
+            int a = 0;
+            rep(l,0,n){
+                while(r<n and m2.size() < c){
+                    m2[v[r]]++;
+                    r++;
+                }
+                
+                if(m2.size()==c) a+=n-r+1;
+                m2[v[l]]--;
+                if(m2[v[l]]==0) m2.erase(v[l]);
+            }
+
+            return a;
+        }
+    };
