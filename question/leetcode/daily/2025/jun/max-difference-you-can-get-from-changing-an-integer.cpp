0f8e2dea5178bd57412a28a913a428ec617f0029
brian.leung <knleungaa@connect.ust.hk>
2025-06-15 12:01:56 -0400

    daily

---
+++ question/leetcode/daily/2025/jun/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+
+typedef pair<int, int> pii;
+typedef vector<pair<int,int>> vii;
+typedef vector<vector<pair<int,int>>> vvii;
+typedef vector<tuple<int, int, int>> viii;
+
+typedef tuple<int, int, int> tiii;
+
+typedef long long ll;
+
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+
+typedef pair<ll, ll> pll;
+typedef vector<pair<ll,ll>> vll;
+typedef vector<vector<pair<ll,ll>>> vvll;
+typedef vector<tuple<ll, ll, ll>> vlll;
+
+typedef tuple<ll, ll, ll> tlll;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define range(i,b) for (int i = 0; i < b; i++)
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define repr(i,a,b) for (int i = a; i > b; i--)
+#define sq(a) (a)*(a)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define ceild(a,b) ((a)%(b) ? ((a)/(b) + 1) : (a)/(b))
+#define TxtIO freopen("input.txt","r",stdin); freopen("output.txt","w",stdout)
+
+const ll mod = 1e9 + 7;
+
+vii dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+vii dirs8 = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+
+
+
+class Solution {
+public:
+    int maxDiff(int v) {
+        string s = to_string(v);
+        int n = s.size();
+
+        char from;
+
+        rep(i,0,n){
+            if(s[i]!='9'){
+                from = s[i];
+                break;
+            }
+        }
+
+        string s1 = s;
+        rep(i,0,n){
+            if(s1[i]==from) s1[i] = '9';
+        }
+
+        char to;
+        if(s[0]!='1'){
+            from = s[0];
+            to = '1';
+        }
+        else{
+            from = '1';
+            to = '1';
+            rep(i,0,n){
+                if(s[i]!='1' and s[i]!='0'){
+                    from = s[i];
+                    to = '0';
+                    break;
+                }
+            }
+        }
+        
+        string s2 = s;
+        rep(i,0,n){
+            if(s2[i]==from) s2[i] = to;
+        }        
+
+        return stoi(s1) - stoi(s2);
+    }
+};
